a4d4c2c6eaee21ef55e74c219a4432ca4cda2002
HannsHau <bierkoenig@web.de>
2026-03-23 10:33:17 +0100

    fix

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>
-        Open up App.js to start working on your app!, Oh it is really real time!!! without rocket emoij :-(
+        Open up App.js to start working on your app!, Oh it is really real time!!! without rocket emoij 
       </Text>
       <StatusBar style="auto" />
     </View>
